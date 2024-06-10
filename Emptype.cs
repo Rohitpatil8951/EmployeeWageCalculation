610510eabbc30ae932eeddcbdc807bb372e5eb6c
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-10 16:36:04 +0530

    This checks emp type

---
+++ Emptype.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeWage
+{
+    public class Emptype
+    {
+        static void Main(string[] args)
+        {
+
+            int partTimeEmp = 4;
+            int fullTimeEmp = 8;
+            if (partTimeEmp == 4)
+            {
+                Console.WriteLine("Part time employe");
+            }
+            else if (partTimeEmp == 8)
+            {
+                Console.WriteLine("Full time employe");
+
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid hours");
+            }
+
+        }
+    }
+}
