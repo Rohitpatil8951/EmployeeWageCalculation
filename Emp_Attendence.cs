fa3596b0d9c4125e782261d6981556fa883426da
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-10 15:15:02 +0530

    Attedence wage of employee

--- Emp_Attendence.cs
+++ Emp_Attendence.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace EmployeeWage
-//{
-//    public class Emp_Attendence
-//    {
-//        static void Main(string[] args)
-//        {
-//            int Emp_Attendence;
-//            Random random = new Random();
-//            Emp_Attendence = random.Next(2);
-//            Console.WriteLine(Emp_Attendence);
-//            if (Emp_Attendence == 0)
-//            {
-//                Console.WriteLine("Employe abesent");
+namespace EmployeeWage
+{
+    public class Emp_Attendence
+    {
+        static void Main(string[] args)
+        {
+            int Emp_Attendence;
+            Random random = new Random();
+            Emp_Attendence = random.Next(2);
+            Console.WriteLine(Emp_Attendence);
+            if (Emp_Attendence == 0)
+            {
+                Console.WriteLine("Employe abesent");
 
-//            }
-//            else
-//            {
-//                Console.WriteLine("Employe present");
-//            }
-//        }
+            }
+            else
+            {
+                Console.WriteLine("Employe present");
+            }
+        }
 
-//    }
-//}
+    }
+}
