5c1b1b24187b40d90f582a55f898b111b19035cf
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-10 13:30:41 +0530

    Check wheather employee is present or not

---
+++ Emp_Attendence.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace EmployeeWage
+//{
+//    public class Emp_Attendence
+//    {
+//        static void Main(string[] args)
+//        {
+//            int Emp_Attendence;
+//            Random random = new Random();
+//            Emp_Attendence = random.Next(2);
+//            Console.WriteLine(Emp_Attendence);
+//            if (Emp_Attendence == 0)
+//            {
+//                Console.WriteLine("Employe abesent");
+
+//            }
+//            else
+//            {
+//                Console.WriteLine("Employe present");
+//            }
+//        }
+
+//    }
+//}
