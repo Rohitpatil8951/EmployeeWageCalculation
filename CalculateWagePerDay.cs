03f340080efacfd0bfb1a36a6df45b0fbdde0267
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-10 13:37:39 +0530

    Calculate daily wage of employee

---
+++ CalculateWagePerDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{   
+    public class CalculateWagePerDay
+    {
+        static void Main(string[] args)
+        {
+            int empHoursPerDay = 8;
+            int empWagePerhour = 20;
+            double TotalWagePerday = empHoursPerDay * empWagePerhour;
+            Console.WriteLine(TotalWagePerday);
+        }
+    }
+}
